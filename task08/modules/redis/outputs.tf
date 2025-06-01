68334678db4dcbb9ba46ddd20790658d6e7bacfa
Megha0404 <megha_kumari1@epam.com>
2025-06-01 21:04:39 +0530

    reddis content

--- task08/modules/redis/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "redis_hostname" {
-  description = "The hostname of the Azure Redis Cache instance"
-  value       = azurerm_redis_cache.redis.hostname
-}
-output "redis_primary_key" {
-  description = "The primary access key for the Azure Redis Cache instance"
-  value       = azurerm_redis_cache.redis.primary_access_key
-  sensitive   = true
-}
